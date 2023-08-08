4dc71caef151e0da00e803bc36d4eb939bbe5620
Jauhar Goga <jauhargoga@gmail.com>
2023-08-08 18:45:35 +1200

    Restarted, Finished Nav Bar
    restarted as I was having trouble with nav bar. After this I completed nav bar and started working on hiring page

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,7 +1,1 @@
-//Selector for nav bar
-let nav_header = document.querySelector(".nav_header");
-let navigation_menu = document.querySelector(".navigation_menu");
 
-navigation_menu.addEventListener("click", function () {
-  nav_header.classlist.toggle("menu-open");
-});
